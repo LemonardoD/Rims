08b5faec8e0f2af76e904d938ca4ad01ad46e91b
David Hordiienko <neonz96@mail.ru>
2023-07-18 21:32:05 +0300

    migration add & methods change

--- src/routers/rimsRouter.ts
+++ src/routers/rimsRouter.ts
@@ -7,7 +7,8 @@ rimsRtr.use(cookieParser());
 
 rimsRtr.get("/rims-configs", RimInfo.tryCatch(RimInfo.rimConfigs));
 rimsRtr.get("/rims-popular", RimInfo.tryCatch(RimInfo.rimsPopular));
-rimsRtr.get("/rim-by-id", RimInfo.tryCatch(RimInfo.rimIdVal), RimInfo.tryCatch(RimInfo.rimById));
-rimsRtr.get("/rims-by-brand", RimInfo.tryCatch(RimInfo.rimsBrandVal), RimInfo.tryCatch(RimInfo.rimsByBrands));
+
+rimsRtr.post("/rims-by-brand", RimInfo.tryCatch(RimInfo.rimsBrandVal), RimInfo.tryCatch(RimInfo.rimsByBrands));
+rimsRtr.post("/rim-by-id", RimInfo.tryCatch(RimInfo.rimIdVal), RimInfo.tryCatch(RimInfo.rimById));
 
 export default rimsRtr;
--- src/routers/usOrdersRouter.ts
+++ src/routers/usOrdersRouter.ts
@@ -5,8 +5,8 @@ import UsOrders from "../controllers/workWithCusContr";
 const usOrders = Router();
 usOrders.use(cookieParser());
 
-usOrders.get("/order", UsOrders.tryCatch(UsOrders.CusVal), UsOrders.tryCatch(UsOrders.order));
-usOrders.get("/order-phcall", UsOrders.tryCatch(UsOrders.CusPhVal), UsOrders.tryCatch(UsOrders.orderAPhoneCall));
-usOrders.get("/order-question", UsOrders.tryCatch(UsOrders.CusQuestVal), UsOrders.tryCatch(UsOrders.orderAQuestion));
+usOrders.post("/order", UsOrders.tryCatch(UsOrders.CusVal), UsOrders.tryCatch(UsOrders.order));
+usOrders.post("/order-phcall", UsOrders.tryCatch(UsOrders.CusPhVal), UsOrders.tryCatch(UsOrders.orderAPhoneCall));
+usOrders.post("/order-question", UsOrders.tryCatch(UsOrders.CusQuestVal), UsOrders.tryCatch(UsOrders.orderAQuestion));
 
 export default usOrders;
